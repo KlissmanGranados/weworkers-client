f7ab085eaa80a4d03a4fd22b44b3c6a7c7729e38
KlissmanGranados <granadosklissman@gmail.com>
2021-12-28 21:59:58 -0400

    deploy

--- main.js
+++ main.js
@@ -7,7 +7,7 @@
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /home/runner/work/WeWorkers/WeWorkers/src/main.ts */"zUnb");
+module.exports = __webpack_require__(/*! /home/klissmar/Documentos/WeWorkers/src/main.ts */"zUnb");
 
 
 /***/ }),
